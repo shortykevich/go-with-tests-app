7ade241feaba166ebefa054282cf903e9225891f
shortyk <shortykofficial@gmail.com>
2025-04-03 19:38:32 +0300

    added actual blind raise scheduling

--- cli/cli.go
+++ cli/cli.go
@@ -41,7 +41,14 @@ func NewCLI(store leaguedb.PlayersStorage, in io.Reader, alerter BlindAlerter) *
 }
 
 func (c *CLI) PlayPoker() {
-	c.alerter.ScheduleAlertAt(5*time.Second, 100)
+	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
+	blindTime := 0 * time.Second
+
+	for _, blind := range blinds {
+		c.alerter.ScheduleAlertAt(blindTime, blind)
+		blindTime = blindTime + 10*time.Minute
+	}
+
 	userInput := c.readInput()
 	c.storage.PostPlayerScore(getTheName(userInput))
 }
--- cli/cli_test.go
+++ cli/cli_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 	"testing"
+	"time"
 
 	tutils "github.com/shortykevich/go-with-tests-app/tests/utils"
 )
@@ -36,8 +37,35 @@ func TestBlindAerter(t *testing.T) {
 		cli := NewCLI(playerStorage, in, blindAlerter)
 		cli.PlayPoker()
 
-		if len(blindAlerter.alerts) != 1 {
-			t.Fatal("expected a blind alert to be scheduled")
+		cases := []scheduledAlert{
+			{0 * time.Second, 100},
+			{10 * time.Minute, 200},
+			{20 * time.Minute, 300},
+			{30 * time.Minute, 400},
+			{40 * time.Minute, 500},
+			{50 * time.Minute, 600},
+			{60 * time.Minute, 800},
+			{70 * time.Minute, 1000},
+			{80 * time.Minute, 2000},
+			{90 * time.Minute, 4000},
+			{100 * time.Minute, 8000},
+		}
+
+		for i, c := range cases {
+			t.Run(fmt.Sprintf("%d scheduled for %v", c.amount, c.at), func(t *testing.T) {
+				if len(blindAlerter.alerts) <= i {
+					t.Fatalf("alert %d was not scheduled %v", i, blindAlerter.alerts)
+				}
+
+				alert := blindAlerter.alerts[i]
+
+				if alert.amount != c.amount {
+					t.Errorf("got amount %d, want %d", alert.amount, c.amount)
+				}
+				if alert.at != c.at {
+					t.Errorf("got scheduled time of %v, want %v", alert.at, c.at)
+				}
+			})
 		}
 	})
 }
